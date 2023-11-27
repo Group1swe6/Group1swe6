015b3538c3a05be38a98fb654b79f81144568774
Group1swe6 <143681015+Group1swe6@users.noreply.github.com>
2023-11-27 06:29:30 +0600

    Update delete_route.php

--- csci361teamwork/admin/delete_route.php
+++ csci361teamwork/admin/delete_route.php
@@ -11,16 +11,12 @@ $route_id=$_GET['route_id'];
 $strSQL ="delete from route WHERE route_id = '$route_id'";
 $result=$mysqli->query($strSQL);
 IF($result){	
-echo "  <div class='container'>
-      <h2>Data was successfully deleted.</h2>
-      <a href='servicesAdmin.php'>Go back to database</a>
-    </div>";
-} ELSE {
-echo "<div class='container'>
-      <h2>Oops! Page not found.</h2>
-      <h1>404</h1>
-      <p>Something went wrong.</p>
-      <a href='servicesAdmin.php'>Go back to database</a>
-    </div>";	
+  echo "<script>alert('Successfully deleted');
+  window.location.href = 'servicesAdmin.php';</script>";
+} else {
+  echo "<script>
+          alert('Cannot be deleted');
+          window.location.href = 'servicesAdmin.php';
+        </script>";
 }
 ?>
