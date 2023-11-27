c06c89369898826b06eeba23612b290fb0713907
Group1swe6 <143681015+Group1swe6@users.noreply.github.com>
2023-11-27 06:37:27 +0600

    Update edit_task_process.php

--- csci361teamwork/admin/edit_task_process.php
+++ csci361teamwork/admin/edit_task_process.php
@@ -1,33 +1,37 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <link rel="stylesheet">
-  </head>
-  <body>
 <?php
-    $mysqli = new mysqli("localhost", "root", "", "db");
-    $id=$_POST['id'];
-    $description=$_POST['description'];
-    $driver_id=$_POST['driver_id'];
-    $vehicle_id=$_POST['vehicle_id'];
-    $status_of_task=$_POST['status_of_task'];
-    $total_time=$_POST['total_time'];
-    $result=$mysqli-> query("update task set description='$description', driver_id='$driver_id', vehicle_id='$vehicle_id', status_of_task='$status_of_task', total_time='$total_time' where id='$id'");
-    IF(!$result){
-        echo "<div class='container'>
-            <h2>Oops! Page not found.</h2>
-            <h1>404</h1>
-            <p>Something has go wrong.</p>
-            <a href='edit_task.php'>Go back for edition</a>
-            </div>";
-    } ELSE {
-        echo "<div class='container'>
-        <h2>Edition was successfully completed.</h2>
-        <a href='tasks.php'>Go back to database</a>
-        </div>";
-    }
+$mysqli = new mysqli("localhost", "root", "", "db");
+
+// Check for connection errors
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+    exit();
+}
+
+$description = $_POST['description'];
+$driver_id = $_POST['driver_id'];
+$vehicle_id = $_POST['vehicle_id'];
+$status_of_task = $_POST['status_of_task'];
+$total_time = $_POST['total_time'];
+
+// Ensure you're using 'maint_job_id' as the key
+if (isset($_POST['id'])) {
+    $id = $_POST['id'];
+
+    // Prepared statement to prevent SQL injection
+    $query = $mysqli -> query("UPDATE task 
+              SET description='$description', driver_id='$driver_id', vehicle_id='$vehicle_id',status_of_task='$status_of_task',total_time='$total_time'
+              WHERE id='$id'");
+          IF($query){ 
+            echo "<script> 
+                  alert('successfully edited')
+                  window.location.href = 'tasks.php' 
+                  </script>"; 
+            } ELSE { 
+              echo "<script> 
+                  alert('incorrect data')
+                  window.location.href = 'edit_task.php' 
+                  </script>"; 
+              } 
+            }
+$mysqli->close();
 ?>
-  </body>
-</html>
