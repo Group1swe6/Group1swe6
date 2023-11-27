3e9db0d42329cc3d1e006f5312f2659c662f2ba7
Group1swe6 <143681015+Group1swe6@users.noreply.github.com>
2023-11-27 06:36:58 +0600

    Update edit_route_process.php

--- csci361teamwork/admin/edit_route_process.php
+++ csci361teamwork/admin/edit_route_process.php
@@ -1,34 +1,38 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <link rel="stylesheet">
-  </head>
-  <body>
 <?php
-    $mysqli = new mysqli("localhost", "root", "", "db");
-    $route_id=$_POST['route_id'];
-    $date_of_route=$_POST['date_of_route'];
-    $curr_location=$_POST['curr_location'];
-    $start_location=$_POST['start_location'];
-    $end_location=$_POST['end_location'];
-    $status_of_route=$_POST['status_of_route'];
-    $driver_id=$_POST['driver_id'];
-    $result=$mysqli-> query("update route set date_of_route='$date_of_route', curr_location='$curr_location', start_location='$start_location', end_location='$end_location', status_of_route='$status_of_route', driver_id='$driver_id' where route_id='$route_id'");
-    IF(!$result){
-        echo "<div class='container'>
-            <h2>Oops! Page not found.</h2>
-            <h1>404</h1>
-            <p>Something has go wrong.</p>
-            <a href='edit_route.php'>Go back for edition</a>
-            </div>";
-    } ELSE {
-        echo "<div class='container'>
-        <h2>Edition was successfully completed.</h2>
-        <a href='servicesAdmin.php'>Go back to database</a>
-        </div>";
-    }
+$mysqli = new mysqli("localhost", "root", "", "db");
+
+// Check for connection errors
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+    exit();
+}
+
+$date_of_route = $_POST['date_of_route'];
+$curr_location = $_POST['curr_location'];
+$start_location = $_POST['start_location'];
+$end_location = $_POST['end_location'];
+$status_of_route = $_POST['status_of_route'];
+$driver_id = $_POST['driver_id'];
+
+// Ensure you're using 'maint_job_id' as the key
+if (isset($_POST['route_id'])) {
+    $route_id = $_POST['route_id'];
+
+    // Prepared statement to prevent SQL injection
+    $query = $mysqli -> query("UPDATE route 
+              SET date_of_route='$date_of_route', curr_location='$curr_location', start_location='$start_location', end_location='$end_location', status_of_route='$status_of_route', driver_id='$driver_id'
+              WHERE route_id='$route_id'");
+          IF($query){ 
+            echo "<script> 
+                  alert('successfully edited')
+                  window.location.href = 'servicesAdmin.php' 
+                  </script>"; 
+            } ELSE { 
+              echo "<script> 
+                  alert('incorrect data')
+                  window.location.href = 'edit_route.php' 
+                  </script>"; 
+              } 
+            }
+$mysqli->close();
 ?>
-  </body>
-</html>
