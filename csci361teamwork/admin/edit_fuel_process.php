3d48f07b02994a5afff066d43a5d8e1aa9de7bda
Group1swe6 <143681015+Group1swe6@users.noreply.github.com>
2023-11-27 06:35:55 +0600

    Update edit_fuel_process.php

--- csci361teamwork/admin/edit_fuel_process.php
+++ csci361teamwork/admin/edit_fuel_process.php
@@ -1,32 +1,37 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <link rel="stylesheet">
-  </head>
-  <body>
 <?php
-    $mysqli = new mysqli("localhost", "root", "", "db");
-    $driver_id=$_POST['fuel_person_id'];
-    $name=$_POST['name'];
-    $phone=$_POST['phone'];
-    $email=$_POST['email'];
-    $fuel_job_id=$_POST['fuel_job_id'];
-    $result=$mysqli-> query("update fuelingperson set name='$name', phone='$phone', email='$email', fuel_job_id='$fuel_job_id' where fuel_person_id='$fuel_person_id'");
-    IF(!$result){
-        echo "<div class='container'>
-            <h2>Oops! Page not found.</h2>
-            <h1>404</h1>
-            <p>Something has go wrong.</p>
-            <a href='edit_fuel.php'>Go back for edition</a>
-            </div>";
-    } ELSE {
-        echo "<div class='container'>
-        <h2>Edition was successfully completed.</h2>
-        <a href='fuelling.php'>Go back to database</a>
-        </div>";
-    }
+$mysqli = new mysqli("localhost", "root", "", "db");
+
+// Check for connection errors
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+    exit();
+}
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$maint_person_id = $_POST['maint_person_id'];
+$fuel_job_id = $_POST['fuel_job_id'];
+
+// Ensure you're using 'maint_job_id' as the key
+if (isset($_POST['fuel_job_id'])) {
+    $fuel_job_id = $_POST['fuel_job_id'];
+
+    // Prepared statement to prevent SQL injection
+    $query = $mysqli -> query("UPDATE fuelingperson 
+              SET name='$name', phone='$phone', email='$email', fuel_job_id='$fuel_job_id'
+              WHERE fuel_person_id='$fuel_job_id'");
+          IF($query){ 
+            echo "<script> 
+                  alert('successfully edited')
+                  window.location.href = 'fuelling.php' 
+                  </script>"; 
+            } ELSE { 
+              echo "<script> 
+                  alert('incorrect data')
+                  window.location.href = 'edit_fuel.php' 
+                  </script>"; 
+              } 
+            }
+$mysqli->close();
 ?>
-  </body>
-</html>
