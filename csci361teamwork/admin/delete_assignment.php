ee757324502b8572b625ccd36b75ca9c784a866e
Group1swe6 <143681015+Group1swe6@users.noreply.github.com>
2023-11-27 06:28:21 +0600

    Update delete_assignment.php

--- csci361teamwork/admin/delete_assignment.php
+++ csci361teamwork/admin/delete_assignment.php
@@ -11,16 +11,12 @@ $assignment_id=$_GET['assignment_id'];
 $strSQL ="delete from assignments WHERE assignment_id = '$assignment_id'";
 $result=$mysqli->query($strSQL);
 IF($result){	
-echo "  <div class='container'>
-      <h2>Data was successfully deleted.</h2>
-      <a href='assignment.php'>Go back to database</a>
-    </div>";
-} ELSE {
-echo "<div class='container'>
-      <h2>Oops! Page not found.</h2>
-      <h1>404</h1>
-      <p>Something went wrong.</p>
-      <a href='assignment.php'>Go back to database</a>
-    </div>";	
+  echo "<script>alert('Successfully deleted');
+  window.location.href = 'assignment.php';</script>";
+} else {
+  echo "<script>
+          alert('Cannot be deleted');
+          window.location.href = 'assignment.php';
+        </script>";
 }
 ?>
