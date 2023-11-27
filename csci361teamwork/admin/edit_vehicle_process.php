13d023fbc82587b271ac0f874806a092a0f01d3e
Group1swe6 <143681015+Group1swe6@users.noreply.github.com>
2023-11-27 06:37:55 +0600

    Update edit_vehicle_process.php

--- csci361teamwork/admin/edit_vehicle_process.php
+++ csci361teamwork/admin/edit_vehicle_process.php
@@ -1,35 +1,39 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <link rel="stylesheet">
-  </head>
-  <body>
 <?php
-    $mysqli = new mysqli("localhost", "root", "", "db");
-    $vehicle_id=$_POST['vehicle_id'];
-    $make=$_POST['make'];
-    $plate_number=$_POST['plate_number'];
-    $model=$_POST['model'];
-    $year_of_vehicle=$_POST['year_of_vehicle'];
-    $sitting_capacity=$_POST['sitting_capacity'];
-    $status_of_vehicle=$_POST['status_of_vehicle'];
-    $mileage=$_POST['mileage'];
-    $result=$mysqli-> query("update vehicle set make='$make', plate_number='$plate_number', model='$model', year_of_vehicle='$year_of_vehicle', sitting_capacity='$sitting_capacity', status_of_vehicle='$status_of_vehicle', mileage='$mileage' where vehicle_id='$vehicle_id'");
-    IF(!$result){
-        echo "<div class='container'>
-            <h2>Oops! Page not found.</h2>
-            <h1>404</h1>
-            <p>Something has go wrong.</p>
-            <a href='edit_vehicle.php'>Go back for edition</a>
-            </div>";
-    } ELSE {
-        echo "<div class='container'>
-        <h2>Edition was successfully completed.</h2>
-        <a href='vehicle.php'>Go back to database</a>
-        </div>";
-    }
+$mysqli = new mysqli("localhost", "root", "", "db");
+
+// Check for connection errors
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+    exit();
+}
+
+$make = $_POST['make'];
+$plate_number = $_POST['plate_number'];
+$model = $_POST['model'];
+$year_of_vehicle = $_POST['year_of_vehicle'];
+$sitting_capacity = $_POST['sitting_capacity'];
+$status_of_vehicle = $_POST['status_of_vehicle'];
+$mileage = $_POST['mileage'];
+
+// Ensure you're using 'maint_job_id' as the key
+if (isset($_POST['vehicle_id'])) {
+    $vehicle_id = $_POST['vehicle_id'];
+
+    // Prepared statement to prevent SQL injection
+    $query = $mysqli -> query("UPDATE vehicle 
+              SET make='$make', plate_number='$plate_number', model='$model',year_of_vehicle='$year_of_vehicle',sitting_capacity='$sitting_capacity',status_of_vehicle='$status_of_vehicle',mileage='$mileage'
+              WHERE vehicle_id='$vehicle_id'");
+          IF($query){ 
+            echo "<script> 
+                  alert('successfully edited')
+                  window.location.href = 'vehicle.php' 
+                  </script>"; 
+            } ELSE { 
+              echo "<script> 
+                  alert('incorrect data')
+                  window.location.href = 'edit_vehicle.php' 
+                  </script>"; 
+              } 
+            }
+$mysqli->close();
 ?>
-  </body>
-</html>
