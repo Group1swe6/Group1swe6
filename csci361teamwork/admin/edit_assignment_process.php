06579ab30d1e31e33597785cca45ec6123d82fb0
Group1swe6 <143681015+Group1swe6@users.noreply.github.com>
2023-11-27 06:33:16 +0600

    Update edit_assignment_process.php

--- csci361teamwork/admin/edit_assignment_process.php
+++ csci361teamwork/admin/edit_assignment_process.php
@@ -1,30 +1,34 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <link rel="stylesheet">
-  </head>
-  <body>
 <?php
-    $mysqli = new mysqli("localhost", "root", "", "db");
-    $assignment_id=$_POST['assignment_id'];
-    $driver_id=$_POST['driver_id'];
-    $vehicle_id=$_POST['vehicle_id'];
-    $result=$mysqli-> query("update assignments set driver_id='$driver_id', vehicle_id='$vehicle_id' where assignment_id='$assignment_id'");
-    IF(!$result){
-        echo "<div class='container'>
-            <h2>Oops! Page not found.</h2>
-            <h1>404</h1>
-            <p>Something has go wrong.</p>
-            <a href='edit_assignment.php'>Go back for edition</a>
-            </div>";
-    } ELSE {
-        echo "<div class='container'>
-        <h2>Edition was successfully completed.</h2>
-        <a href='assignment.php'>Go back to database</a>
-        </div>";
-    }
+$mysqli = new mysqli("localhost", "root", "", "db");
+
+// Check for connection errors
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+    exit();
+}
+
+$driver_id = $_POST['driver_id'];
+$vehicle_id = $_POST['vehicle_id'];
+
+// Ensure you're using 'maint_job_id' as the key
+if (isset($_POST['assignment_id'])) {
+    $assignment_id = $_POST['assignment_id'];
+
+    // Prepared statement to prevent SQL injection
+    $query = $mysqli -> query("UPDATE assignments
+              SET driver_id='$driver_id', vehicle_id='$vehicle_id'
+              WHERE assignment_id='$assignment_id'");
+          IF($query){ 
+            echo "<script> 
+                  alert('successfully edited')
+                  window.location.href = 'assignment.php' 
+                  </script>"; 
+            } ELSE { 
+              echo "<script> 
+                  alert('incorrect data')
+                  window.location.href = 'edit_assignment.php' 
+                  </script>"; 
+              } 
+            }
+$mysqli->close();
 ?>
-  </body>
-</html>
