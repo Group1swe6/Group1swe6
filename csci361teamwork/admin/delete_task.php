069c1c7797a24d86aaf24d633d3aa48bd7d616ff
Group1swe6 <143681015+Group1swe6@users.noreply.github.com>
2023-11-27 06:29:46 +0600

    Update delete_task.php

--- csci361teamwork/admin/delete_task.php
+++ csci361teamwork/admin/delete_task.php
@@ -11,16 +11,12 @@ $id=$_GET['id'];
 $strSQL ="delete from task WHERE id = '$id'";
 $result=$mysqli->query($strSQL);
 IF($result){	
-echo "  <div class='container'>
-      <h2>Data was successfully deleted.</h2>
-      <a href='tasks.php'>Go back to database</a>
-    </div>";
-} ELSE {
-echo "<div class='container'>
-      <h2>Oops! Page not found.</h2>
-      <h1>404</h1>
-      <p>Something went wrong.</p>
-      <a href='tasks.php'>Go back to database</a>
-    </div>";	
+  echo "<script>alert('Successfully deleted');
+  window.location.href = 'tasks.php';</script>";
+} else {
+  echo "<script>
+          alert('Cannot be deleted');
+          window.location.href = 'tasks.php';
+        </script>";
 }
 ?>
