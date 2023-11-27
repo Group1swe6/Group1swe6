437ebf60d8a6441d3723c6ec329dbc65e01dc155
Group1swe6 <143681015+Group1swe6@users.noreply.github.com>
2023-11-27 06:36:29 +0600

    Update edit_maint_process.php

--- csci361teamwork/admin/edit_maint_process.php
+++ csci361teamwork/admin/edit_maint_process.php
@@ -1,32 +1,37 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <link rel="stylesheet">
-  </head>
-  <body>
 <?php
-    $mysqli = new mysqli("localhost", "root", "", "db");
-    $driver_id=$_POST['maint_person_id'];
-    $name=$_POST['name'];
-    $phone=$_POST['phone'];
-    $email=$_POST['email'];
-    $maintenance_job_id=$_POST['maintenance_job_id'];
-    $result=$mysqli-> query("update fuelingperson set name='$name', phone='$phone', email='$email', maintenance_job_id='$maintenance_job_id' where maint_person_id='$maint_person_id'");
-    IF(!$result){
-        echo "<div class='container'>
-            <h2>Oops! Page not found.</h2>
-            <h1>404</h1>
-            <p>Something has go wrong.</p>
-            <a href='edit_maint.php'>Go back for edition</a>
-            </div>";
-    } ELSE {
-        echo "<div class='container'>
-        <h2>Edition was successfully completed.</h2>
-        <a href='maintenance.php'>Go back to database</a>
-        </div>";
-    }
+$mysqli = new mysqli("localhost", "root", "", "db");
+
+// Check for connection errors
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+    exit();
+}
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$maint_person_id = $_POST['maint_person_id'];
+$maint_job_id = $_POST['maint_job_id'];
+
+// Ensure you're using 'maint_job_id' as the key
+if (isset($_POST['maint_job_id'])) {
+    $maintenance_job_id = $_POST['maint_job_id'];
+
+    // Prepared statement to prevent SQL injection
+    $query = $mysqli -> query("UPDATE maintenanceperson 
+              SET name='$name', phone='$phone', email='$email', maint_job_id='$maint_job_id'
+              WHERE maint_person_id='$maint_job_id'");
+          IF($query){ 
+            echo "<script> 
+                  alert('successfully edited')
+                  window.location.href = 'maintenance.php' 
+                  </script>"; 
+            } ELSE { 
+              echo "<script> 
+                  alert('incorrect data')
+                  window.location.href = 'edit_maint.php' 
+                  </script>"; 
+              } 
+            }
+$mysqli->close();
 ?>
-  </body>
-</html>
