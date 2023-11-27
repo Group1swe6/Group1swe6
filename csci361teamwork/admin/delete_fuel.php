577b3ed81918cdd68c59d5140b9afcdb36860248
Group1swe6 <143681015+Group1swe6@users.noreply.github.com>
2023-11-27 06:29:00 +0600

    Update delete_fuel.php

--- csci361teamwork/admin/delete_fuel.php
+++ csci361teamwork/admin/delete_fuel.php
@@ -11,16 +11,12 @@ $fuel_person_id=$_GET['fuel_person_id'];
 $strSQL ="delete from fuelingperson WHERE fuel_person_id = '$fuel_person_id'";
 $result=$mysqli->query($strSQL);
 IF($result){	
-echo "  <div class='container'>
-      <h2>Data was successfully deleted.</h2>
-      <a href='fuelling.php'>Go back to database</a>
-    </div>";
-} ELSE {
-echo "<div class='container'>
-      <h2>Oops! Page not found.</h2>
-      <h1>404</h1>
-      <p>Something went wrong.</p>
-      <a href='fuelling.php'>Go back to database</a>
-    </div>";	
+  echo "<script>alert('Successfully deleted');
+  window.location.href = 'fuelling.php';</script>";
+} else {
+  echo "<script>
+          alert('Cannot be deleted');
+          window.location.href = 'fuelling.php';
+        </script>";
 }
 ?>
